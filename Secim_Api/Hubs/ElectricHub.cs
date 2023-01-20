41298c664497f59db961e47d3bd869a56286d156
Neşe Erbil <96241994+neseerbil1@users.noreply.github.com>
2023-01-20 11:20:01 +0300

    Proje dosyası ekle.

---
+++ Secim_Api/Hubs/ElectricHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Secim_Api.Hubs
+{
+    public class ElectricHub:Hub
+    {
+        private readonly ElectricService _service;
+        public ElectricHub(ElectricService service)
+        {
+            _service = service;
+        }
+
+        public async Task GetElectricConsumeList()
+        {
+            await Clients.All.SendAsync("ReceiveElectricList", "Elektrik Verilerini Al");
+        }
+    }
+}
---
+++ Secim_Api/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Secim_Api.Models
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+           
+        }
+        public DbSet<Electric> Electrics { get; set; }
+
+    }
+
+}
